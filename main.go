0ddf1dd68c6f275499ec13b1be5f09afe753d60c
Vadim Spitsyn <spicyn.v2001@gmail.com>
2025-10-06 12:59:00 +0700

    feat: recieving all merge request for user, started to work on 2 mods - rebase "all" and "one by one"

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -20,10 +21,10 @@ func main() {
 	viper.SetConfigName("update-stale-config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("$HOME/.config/")
+	reader := bufio.NewReader(os.Stdin)
 
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			reader := bufio.NewReader(os.Stdin)
 
 			fmt.Print("Enter your gitlab host (for example \"gitlab.com\"): ")
 			host, _ := reader.ReadString('\n')
@@ -55,6 +56,53 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
-	users, _, err := git.Users.ListUsers(&gitlab.ListUsersOptions{})
-	fmt.Printf("Found %d users", len(users))
+	fmt.Println("Run time GitLab client created")
+
+	currentUser, _, err := git.Users.CurrentUser()
+	if err != nil {
+		log.Fatalf("Failed to get information about owner of token: %v", err)
+	}
+	fmt.Printf("Actions will be performed with rights of the owner of token - \"%s\" (%s)\n", currentUser.Username, currentUser.Email)
+
+	fmt.Printf("List of available projects: \n")
+	projects, _, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
+		OrderBy: gitlab.Ptr("updated_at"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to list projects: %v", err)
+	}
+	for index, p := range projects {
+		fmt.Printf("%d. %s\n", index+1, p.Name)
+	}
+
+	fmt.Println("Choose one project (provide order number): ")
+	pString, _ := reader.ReadString('\n')
+	pString = strings.TrimSpace(pString)
+	projectOrder, err := strconv.Atoi(strings.TrimSpace(pString))
+	if err != nil {
+		log.Fatalf("Error converting project id to int: %v\n", err)
+	}
+	projectIndex := projectOrder - 1
+	if projects[projectIndex] == nil {
+		log.Fatalf("Project does not exist: %v", pString)
+	}
+
+	fmt.Printf("Searching MRs for project ID %d 🔎...", projects[projectIndex].ID)
+	mergeRequests, _, err := git.MergeRequests.ListMergeRequests(&gitlab.ListMergeRequestsOptions{
+		State:    gitlab.Ptr("opened"),
+		AuthorID: gitlab.Ptr(currentUser.ID),
+		Scope:    gitlab.Ptr("all"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(mergeRequests) == 0 {
+		log.Fatal("No open merge requests found 🚫")
+	}
+
+	for _, mr := range mergeRequests {
+		if mr.ProjectID == projects[projectIndex].ID {
+			fmt.Printf("\n%s (%s)", mr.Title, strings.Join(mr.Labels, ", "))
+		}
+	}
 }
